b9c4e9cd838d5b263650230fd113a1d4855f75d2
hemanthnethinti <hemanthnetinti@gmail.com>
2026-01-31 18:41:53 +0530

    setup html document with rss link

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS Feed"
+          href="/rss.xml"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
